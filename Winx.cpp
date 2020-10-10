38e9b06e97beaae679b3a15db46986cb668b86bf
wajishahid10@gmail.com <wajahatdev@gmail.com>
2020-10-10 14:41:29 +0500

    Reassembling Code

---
+++ Winx.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+void printBar()
+{
+	for (int i=0;i<9;i++)
+	cout<<"-";
+	
+	cout<<endl;
+	for (int i=0;i<9;i++)
+{	
+	if (i>2 && i<7)
+	cout<<"-";
+	else
+	cout<<"*";
+}
+cout<<endl;
+}
+
+int main ()
+{
+	printBar();
+	for (int i=0;i<5;i++)
+		cout<<"I love Pakistan."<<endl;
+	
+	cout<<endl;	
+	printBar();
+	bool isTodayFriday;
+	isTodayFriday= true;
+	if(isTodayFriday)
+	cout<<"Bhai Nahana Parey gaa";
+	else
+	cout<<"Khaire hi hia Cheetey";
+}
